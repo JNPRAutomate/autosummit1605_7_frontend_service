39209e39f293ec2967bd5c12d45fcd1dba2e1c9b
rlemm-juniper <rlemm@juniper.net>
2016-05-04 19:51:01 -0400

    modified prison.html and front-end.py

--- front_end.py
+++ front_end.py
@@ -1,30 +1,57 @@
 #!/usr/bin/python
-from docker import Client
 import argparse
 import influxdb
 import docker.tls as tls
-from os import path
 import os
 import pprint
 import subprocess
 import json
 import os.path
-from sys import platform as _platform
 import requests
 import cherrypy
 import multiprocessing.pool
-from threading import Thread
-from flask import *
 import math
 import time
 import sys
 import yaml
+from docker import Client
+from sys import platform as _platform
+from threading import Thread
+from flask import *
+from os import path
 from jinja2 import Template
 from jnpr.junos import Device
 from jnpr.junos.exception import RpcError
-from jnpr.junos.utils.config import Config as junos_config
+from jnpr.junos.utils.config import Config
 from jnpr.junos.factory.factory_loader import FactoryLoader
 
+JinjaTemplate_Campus = Template("""
+security {
+  zones {
+    security-zone DC {
+      address-book {
+        address {{ var1 }} {{ Address }}/32;
+        address-set CAMPUS_USER {
+          address {{ var1 }};
+        }
+      }
+    }
+  }
+}""")
+
+JinjaTemplate_Internet = Template("""
+security {
+  zones {
+    security-zone DC {
+      address-book {
+        address {{ var1 }} {{ Address }}/32;
+        address-set INTERNET_USER {
+          address {{ var1 }};
+        }
+      }
+    }
+  }
+}""")
 
 dport = ''
 parser = argparse.ArgumentParser(add_help=True)
@@ -43,7 +70,6 @@ if args.p:
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -58,14 +84,15 @@ def prvsn():
 
 @app.route('/provision_services', methods=['GET', 'POST'])
 def provision_services():
-    customername = request.form.get('customername')
-    service1 = request.form.get('service1')
+    name = request.form.get('name')
+    ip_address = request.form.get('ip_address')
+    campus_user = request.form.get('campus_user')
     service2 = request.form.get('service2')
     service3 = request.form.get('service3')
     service4 = request.form.get('service4')
     service5 = request.form.get('service5')
     campus = request.form.get('campus')
-    internet = request.form.get('internet')
+    internet_user = request.form.get('internet_user')
     legacy = request.form.get('legacy')
     provisioned_services = ''
     provisioned_services += Markup('''\
@@ -75,8 +102,69 @@ def provision_services():
 </head>
 <body><center>
 Provisioned - <br>''')
-    if service1:
-        provisioned_services += Markup('''Service 1<br>''')
+
+    if campus_user:
+        dev = Device(host='172.25.45.68', user='lab', passwd='jnpr123')
+        cu = Config(dev)
+        print name, ip_address
+        try:
+            dev.open()
+            dev.timeout = 300
+            cu.lock()
+            jinja_input = {'Address': ip_address, 'var1': name}
+            jinja_data = open("jinjafile.conf", "wb")
+            jinja_data.write(JinjaTemplate_Campus.render(**jinja_input))
+            jinja_data.close()
+            rsp = cu.load( template_path="jinjafile.conf", merge=True )
+            cu.commit()
+            cu.unlock()
+
+        except RpcError:
+            msg = "{0} was Skipped due to RPC Error.  Device is not a Juniper SRX Series"
+            print msg
+            dev.close()
+
+        except Exception as err:
+            msg = "{0} was skipped due to unhandled exception.\n{1}"
+            print msg
+            traceback.print_exc(file=sys.stdout)
+
+        dev.close()
+
+        provisioned_services += Markup('''Campus User: '''+name+''' with IP Address: '''+ip_address+'''<br>''')
+
+    if internet_user:
+        dev = Device(host='172.25.45.68', user='lab', passwd='jnpr123')
+        cu = Config(dev)
+        print name, ip_address
+        try:
+            dev.open()
+            dev.timeout = 300
+            cu.lock()
+            jinja_input = {'Address': ip_address, 'var1': name}
+            jinja_data = open("jinjafile.conf", "wb")
+            jinja_data.write(JinjaTemplate_Internet.render(**jinja_input))
+            jinja_data.close()
+            rsp = cu.load( template_path="jinjafile.conf", merge=True )
+            cu.commit()
+            cu.unlock()
+
+        except RpcError:
+            msg = "{0} was Skipped due to RPC Error.  Device is not a Juniper SRX Series"
+            print msg
+            dev.close()
+
+        except Exception as err:
+            msg = "{0} was skipped due to unhandled exception.\n{1}"
+            print msg
+            traceback.print_exc(file=sys.stdout)
+
+        dev.close()
+
+        provisioned_services += Markup('''Internet User'''+name+''' with IP Address: '''+ip_address+'''<br>''')
+
+    if legacy:
+        provisioned_services += Markup('''Legacy<br>''')
 
     if service2:
         provisioned_services += Markup('''Service 2<br>''')
@@ -92,12 +180,6 @@ Provisioned - <br>''')
 
     if campus:
         provisioned_services += Markup('''Campus<br>''')
-
-    if internet:
-        provisioned_services += Markup('''Internet<br>''')
-
-    if legacy:
-        provisioned_services += Markup('''Legacy<br>''')
 
     provisioned_services += Markup('''<button type="button" name="button" class="btn btn-primary btn-lg" onClick="location.href='../'" enabled><strong>Return to Main Page</strong></button><br>''')
     provisioned_services += Markup('''</body></html>''')
